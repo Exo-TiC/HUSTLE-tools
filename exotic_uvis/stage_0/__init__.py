85d7a14488159454ad78cdd48e271b1316222ac4
Abby Boehm <vab55@cornell.edu>
2025-02-14 10:50:26 -0500

    Automatically check for SPT subarray offsets and warn if calibration is at risk

--- exotic_uvis/stage_0/__init__.py
+++ exotic_uvis/stage_0/__init__.py
@@ -2,9 +2,10 @@ __all__ = [
     "get_files_from_mast",
     "collect_and_move_files",
     "locate_target",
+    "check_spt_subarray",
 ]
 
 
 from exotic_uvis.stage_0.get_files_from_mast import get_files_from_mast
 from exotic_uvis.stage_0.collect_and_move_files import collect_and_move_files
-from exotic_uvis.stage_0.locate_target import locate_target
+from exotic_uvis.stage_0.locate_target import locate_target, check_spt_subarray
