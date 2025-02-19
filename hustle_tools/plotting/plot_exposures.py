41d1449484fc152c6574a924d27c2d701adc2ad1
Abby Boehm <vab55@cornell.edu>
2025-02-19 10:42:40 -0500

    HUSTLE-tools rebrand

---
+++ hustle_tools/plotting/plot_exposures.py
@@ -0,0 +1,77 @@
+import os
+
+import matplotlib.pyplot as plt
+
+
+#define plotting parameters
+plt.rc('font', family='serif')
+plt.rc('xtick', labelsize=14)
+plt.rc('ytick', labelsize=14)
+plt.rc('axes', labelsize=14)
+plt.rc('legend',**{'fontsize':11})
+
+
+def plot_exposure(images, line_data = None, scatter_data = None, 
+                  extent = None, title = None, 
+                  min = 1e-3, max = 1e4, mark_size = 30,
+                  show_plot = False, save_plot = False, 
+                  filename = None, output_dir = None):
+    """Function to plot an image given certain parameters.
+
+    Args:
+        images (np.array): images from the obs.images.
+        line_data (list, optional): list of lists of x, y values denoting
+        lines you want to draw on the plot. Defaults to None.
+        scatter_data (list, optional): list of x, y points you want to
+        scatter on the plot. Defaults to None.
+        extent (tuple of float, optional): if not None, defines bounds
+        of array you want to plot. Defaults to None.
+        title (str, optional): title for the plot. Defaults to None.
+        min (int, optional): darkest point for the colormap. Defaults to 1e-3.
+        max (int, optional): brightest point for the colormap. Defaults to 1e4.
+        mark_size (int, optional): size of scatter points, if scatter_data
+        is not None. Defaults to 30.
+        show_plot (bool, optional): whether to interrupt execution to show
+        the user the plot. Defaults to False.
+        save_plot (bool, optional): whether to save this plot. Defaults to False.
+        filename (list of str, optional): name for each plot file. Defaults to None.
+        output_dir (str, optional): where to save the plots to, if save_plots is
+        True. Defaults to None.
+    """
+    
+    for i, data in enumerate(images): 
+
+        image = data.copy()
+        image[image <= 0] = 1e-10 # allows us to use log normalization
+
+        plt.figure(figsize = (20, 4))
+        plt.imshow(image, origin = 'lower', norm='log', 
+                   vmin = min, vmax = max, 
+                   cmap = 'gist_gray', extent = extent)
+        plt.xlabel('Detector x-pixel')
+        plt.ylabel('Detector y-pixel')
+        plt.colorbar()
+
+        if line_data:
+            for line in line_data:
+                plt.plot(line[0], line[1], color='indianred')
+
+        if scatter_data: 
+            plt.scatter(scatter_data[0], scatter_data[1], s = mark_size, color = 'r', marker = '+')
+
+        if title:
+            plt.title(title)
+        
+        if save_plot:
+            plot_dir = os.path.join(output_dir, 'plots')
+            if not os.path.exists(plot_dir):
+                os.makedirs(plot_dir) 
+            filedir = os.path.join(plot_dir, f'{filename[i]}.png')
+            plt.savefig(filedir, bbox_inches = 'tight', dpi = 300)
+        
+        if show_plot:
+            plt.show(block=True)
+
+        plt.close() # save memory
+    
+    return
---
+++ hustle_tools/stage_1/compute_displacements.py
@@ -0,0 +1,214 @@
+from tqdm import tqdm
+
+import numpy as np
+from photutils.centroids import centroid_com
+
+from exotic_uvis.plotting import plot_exposure
+from exotic_uvis.plotting import plot_bkg_stars, plot_0th_order
+
+
+def refine_location(obs, window=20,
+                    verbose=0, show_plots=0, save_plots=0, output_dir=None):
+    """Function to refine the target location in the direct image
+
+    Args:
+        obs (xarray): obs.direct_image contains the direct image of the source.
+        window (int, optional): how far around the source to draw the window
+        for centroiding. Defaults to 20.
+        verbose (int, optional): how detailed you want the printed statements
+        to be. Defaults to 0.
+        show_plots (int, optional): how many plots you want to show.
+        Defaults to 0.
+        save_plots (int, optional): how many plots you want to save.
+        Defaults to 0.
+        output_dir (str, optional): where to save the plots to, if save_plots
+        is greater than 0. Defaults to None.
+    """
+
+    if verbose >= 1:
+        print("Refining location of source in direct image with centroiding...")
+    
+    # copy direct image
+    image = obs.direct_image.data.copy()
+
+     # get estimated target location
+    x_loc = obs.attrs['target_posx']
+    y_loc = obs.attrs['target_posy']
+
+    # if true, calculate the centroid of a window around the initial guess
+    if window:
+        x0, xf = int(x_loc) - window, int(x_loc) + window
+        y0, yf = int(y_loc) - window, int(y_loc) + window
+        
+        sub_image = image[y0:yf, x0:xf]
+        x1, y1 = centroid_com(sub_image)
+
+        obs.attrs['target_posx'] = x0 + x1
+        obs.attrs['target_posy'] = y0 + y1
+
+    # otherwise, just use the default location
+    else:
+        obs.attrs['target_posx'] = x_loc
+        obs.attrs['target_posy'] = y_loc
+        
+
+    if show_plots > 0 or save_plots > 0:
+        plot_exposure([image], 
+                      scatter_data = [[obs.attrs['target_posx']], [obs.attrs['target_posy']]],
+                      title = 'Computed direct image target location',
+                      show_plot=show_plots, save_plot=save_plots, 
+                      output_dir=output_dir, filename = [f'target_location_in_direct_image']) 
+    
+    return 
+
+
+def track_bkgstars(obs, bkg_stars, window = 15,
+                   verbose = 0, show_plots = 0, save_plots = 0, output_dir = None):
+    """Function to compute the x & y displacement of a given background star
+
+    Args:
+        obs (xarray): obs.images contains the images of the stars.
+        bkg_stars (list of list): estimated positions of stars to track.
+        window (int, optional): how far around the star to draw the window
+        for centroiding. Defaults to 15.
+        verbose (int, optional): how detailed you want the printed statements
+        to be. Defaults to 0.
+        show_plots (int, optional): how many plots you want to show.
+        Defaults to 0.
+        save_plots (int, optional): how many plots you want to save.
+        Defaults to 0.
+        output_dir (str, optional): where to save the plots to, if save_plots
+        is greater than 0. Defaults to None.
+
+    Returns:
+        np.array, np.array: relative and average positions of each star in time.
+    """
+
+    # intialize and copy images
+    stars_pos, abs_pos = [], []
+    images = obs.images.data.copy()
+
+    # iterate over all listed background stars
+    for i, pos_init in tqdm(enumerate(bkg_stars),
+                            desc='Tracking background stars... Progress:',
+                            disable=(verbose==0)):
+        
+        # initialize position
+        pos = []
+
+        # get window limits
+        x0, xf = pos_init[0] - window, pos_init[0] + window
+        y0, yf = pos_init[1] - window, pos_init[1] + window
+
+        # iterate over all images
+        for image in images:
+
+            # define region around background star
+            sub_image = image[y0:yf, x0:xf]
+            
+            # compute centroid
+            x1, y1 = centroid_com(sub_image)
+
+            # append location
+            pos.append([x0 + x1, y0 + y1])
+        
+        rel_pos = np.array(pos) - pos[0]
+        
+        if (show_plots==2 or save_plots==2):
+            # save or show a plot of this star
+            plot_exposure([images[0]], scatter_data = [x0 + x1, y0 + y1],
+                          title = 'Position of star #{}'.format(i),
+                          show_plot = (show_plots==2), save_plot = (save_plots==2), 
+                          filename = ['bkg_star_no{}'.format(i)],
+                          output_dir = output_dir)
+
+        # save background star location as a function of time
+        obs["star{}_disp".format(i)] = (("exp_time", "xy"), rel_pos)
+        stars_pos.append(rel_pos)
+        abs_pos.append(pos)
+
+    stars_pos = np.array(stars_pos)
+    mean_pos = np.mean(stars_pos, axis = 0)
+
+    obs["meanstar_disp"] = (("exp_time", "xy"), mean_pos)
+    
+    # if true, plot the calculated displacements
+    if (show_plots > 0) or (save_plots > 0):
+        mean_loc = list(np.mean(abs_pos, axis = 1).transpose())
+        plot_bkg_stars(image, obs.exp_time.data, mean_loc, mean_pos, stars_pos,
+                       show_plot=(show_plots>0), save_plot=(save_plots>0),
+                       output_dir=output_dir)
+    
+    return stars_pos, mean_pos
+
+
+def track_0thOrder(obs, guess,
+                   verbose=0, show_plots=0, save_plots=0, output_dir=None):
+    """Tracks the 0th order through all frames using centroiding.
+
+    Args:
+        obs (xarray): obs.images contains the images.
+        guess (lst of float): initial x, y offset guess for the
+        0th order's location.
+        verbose (int, optional): how detailed you want the printed statements
+        to be. Defaults to 0.
+        show_plots (int, optional): how many plots you want to show.
+        Defaults to 0.
+        save_plots (int, optional): how many plots you want to save.
+        Defaults to 0.
+        output_dir (str, optional): where to save the plots to, if save_plots
+        is greater than 0. Defaults to None.
+
+    Returns:
+        lst of float: location of the direct image in x, y floats.
+    """
+    # update guess to actual location
+    guess[0] += obs.attrs["target_posx"]
+    guess[1] += obs.attrs["target_posy"]
+
+    # unpack guess and integerize it
+    x0, y0 = [int(i) for i in guess]
+
+    # open lists of position
+    X, Y = [], []
+    for k in tqdm(range(obs.images.shape[0]),
+                  desc='Tracking 0th order... Progress:',
+                  disable=(verbose==0)):
+        # open the kth image
+        d = obs.images[k].values
+
+        # clip a window near the guess
+        window = d[y0-70:y0+70,x0-70:x0+70]
+
+        # centroid the window
+        xs, ys = centroid_com(window)
+
+        # Plot solution.
+        if (show_plots > 0 or save_plots > 0):
+            if k == 0:
+                plot_exposure([window],scatter_data=[[xs,],[ys],],
+                              title='Tracked 0th in frame {}'.format(k),
+                              show_plot=(show_plots>0),save_plot=(save_plots>0),
+                              filename=['0th_tracking_frame{}'.format(k)],output_dir=output_dir)
+            elif (show_plots == 2 or save_plots == 2):
+                plot_exposure([window],scatter_data=[[xs,],[ys],],
+                              title='Tracked 0th in frame {}'.format(k),
+                              show_plot=(show_plots==2),save_plot=(save_plots==2),
+                              filename=['0th_tracking_frame{}'.format(k)],output_dir=output_dir)
+
+        # return to native window
+        xs += x0 - 70
+        ys += y0 - 70
+        
+        # take the source
+        X.append(xs)
+        Y.append(ys)
+    
+    if verbose > 0:
+        print("Tracked 0th order in %.0f frames." % obs.images.shape[0])
+    
+    if (show_plots > 0 or save_plots > 0):
+        plot_0th_order(obs.exp_time.data,X,Y,
+                       show_plot=(show_plots>0),save_plot=(save_plots>0),output_dir=output_dir)
+        
+    return X, Y
